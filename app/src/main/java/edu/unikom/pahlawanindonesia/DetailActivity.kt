ea834a94d7622f2ee09ecac418b8a2342eafb898
Rafi A Saputra <rafi.alwisaputra@gmail.com>
2025-05-21 22:25:54 +0700

    first commit

---
+++ app/src/main/java/edu/unikom/pahlawanindonesia/DetailActivity.kt
@@ -0,0 +1,35 @@
+package edu.unikom.pahlawanindonesia
+
+import android.os.Build
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import edu.unikom.pahlawanindonesia.model.Hero
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_detail)
+        val ivHeroImage = findViewById<ImageView>(R.id.img_heroImage)
+        val txtheroName = findViewById<TextView>(R.id.txt_heroName)
+        val txtheroDesc = findViewById<TextView>(R.id.txt_heroDescription)
+
+        val hero = if (Build.VERSION.SDK_INT >= 33 ){
+            intent.getParcelableExtra("HERO_OBJECT",Hero::class.java)
+        }else {
+            @Suppress("DEPRECATION")
+           intent.getParcelableExtra<Hero>("HERO_OBJECT")
+        }
+
+        if (hero != null) {
+            ivHeroImage.setImageResource(hero.foto)
+            txtheroName.text = hero.nama
+            txtheroDesc.text = hero.deskripsi
+        }
+    }
+}
